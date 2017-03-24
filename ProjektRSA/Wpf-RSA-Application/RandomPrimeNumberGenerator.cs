2fd7c97d50d43f9545f6d85bbfcb5288ce4f5e44
Kamil Witkowski <kawitkowski@sygnity.pl>
2017-03-24 16:23:53 +0100

    Dodano sito erastotenesa

---
+++ ProjektRSA/Wpf-RSA-Application/RandomPrimeNumberGenerator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace Wpf_RSA_Application
+{
+    class RandomPrimeNumberGenerator
+    {
+        private Random random;
+        private long primeNumber;
+        public RandomPrimeNumberGenerator()
+        {
+            primeNumber = generatePrimeNumber();
+        }
+
+        public long generatePrimeNumber()
+        {
+            SieveofEratosthenes s = new SieveofEratosthenes();
+            List<long> primeNumbersList = s.RunAlgorithm();
+            int primeNumbersCount = primeNumbersList.Count();
+            random = new Random();
+            return primeNumbersList.ElementAt(random.Next(primeNumbersCount-1000, primeNumbersCount+1)); // Inclusive with primeNumbersCount
+        }
+
+
+
+
+
+
+    }
+}
---
+++ ProjektRSA/Wpf-RSA-Application/SieveofEratosthenes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_RSA_Application
+{
+    class SieveofEratosthenes
+    {
+        private long n = 9 *1234567l;
+        private bool[] tableOfNumbers;
+        private List<long> primeNumbersList;
+
+        public List<long> RunAlgorithm()
+        {
+            primeNumbersList = new List<long>();
+            tableOfNumbers = new bool[n];//by default they're all false
+
+            for (uint i = 2; i < n; i++)
+            {
+                tableOfNumbers[i] = true;//set all numbers to true
+            }
+            //weed out the non primes by finding mutiples 
+            for (uint j = 2; j < n; j++)
+            {
+                if (tableOfNumbers[j])//is true
+                {
+                    for (long p = 2; (p * j) < n; p++)
+                    {
+                        tableOfNumbers[p * j] = false;
+                    }
+                }
+            }
+
+            for (long i = 0; i < tableOfNumbers.LongLength; i++)
+            {
+                if (tableOfNumbers[i] == true)
+                {
+                    primeNumbersList.Add(i);
+                }
+            }
+
+            return primeNumbersList;
+            //Uptill here e[] sorta of contains a list of primes
+            //the index represent the actual number and the value at the index represents if the number is prime
+            //Example:
+            //e[4], e[100] will all be false since 2,4,100 are not primes
+            //e[5], e[7], e[11], e[13] will all be true because 5,7,11,13 are all prime numbers
+        }
+    }
+}
