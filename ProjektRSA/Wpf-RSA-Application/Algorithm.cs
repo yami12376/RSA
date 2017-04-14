63816ab4279f55edd4d2a64fcdc5d941a8780a53
Kamil Witkowski <kawitkowski@sygnity.pl>
2017-04-14 10:56:07 +0200

    Dodanie klasy Liczenie funkcji Eulera Liczenie iloczynu liczb pierwszych Dodanie TODO - co zostało do zrobienia w ramach algorytmu

---
+++ ProjektRSA/Wpf-RSA-Application/Algorithm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_RSA_Application
+{
+    class Algorithm
+    {
+        private RandomPrimeNumberGenerator randomPrimeNumberGenerator;
+        public void runAlghoritm()
+        {
+            randomPrimeNumberGenerator = new RandomPrimeNumberGenerator();
+            List<long> primeNumbers =  randomPrimeNumberGenerator.generateTwoPrimeNumbers();
+            long n = multiplicationProduct(primeNumbers);
+            long eulerN = countEuler(primeNumbers);
+
+            //TODO 
+            //https://pl.wikipedia.org/wiki/RSA_(kryptografia)
+            //Wybieramy liczbę e(1 < e < φ(n)) względnie pierwszą z φ(n)
+            //Znajdujemy liczbę d, gdzie jej różnica z odwrotnością liczby e jest podzielna przez φ(n) :
+            //d ≡ e−1(mod φ(n))
+            //Ta liczba może być też prościej określona wzorem:
+            //d⋅e ≡ 1(mod φ(n))
+            //Klucz publiczny jest definiowany jako para liczb(n, e), natomiast kluczem prywatnym jest para(n, d).
+            // Szyfrowanie i deszyfrowanie
+
+        }
+
+        private long multiplicationProduct(List<long> primeNumbers)
+        {
+            long result = 1;
+
+            foreach (var primeNumber in primeNumbers)
+            {
+                result *= primeNumber;
+            }
+            return result;
+        }
+
+
+        private long countEuler(List<long> primeNumbers)
+        {
+            long result = 1;
+
+            foreach (var primeNumber in primeNumbers)
+            {
+                result *= (primeNumber-1);
+            }
+            return result;
+        }
+        
+
+    }
+}
